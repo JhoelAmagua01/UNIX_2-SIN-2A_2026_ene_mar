d6c3530a4e83179cf72f837a441a1bd25726584d
JhoelAmagua01 <144384893+JhoelAmagua01@users.noreply.github.com>
2026-01-27 01:56:55 +0000

    wip: cp and mv

---
+++ semana4/test/script_s4.sh
@@ -0,0 +1,10 @@
+ mkdir -p padre_ja/hijo_ja
+ pwd
+ cd
+ pwd
+ cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_ja/hijo_ja
+ touch archivo_ja.txt
+ echo "0" > archivo_ja.txt
+ echo "1" >> archivo_ja.txt
+ echo "2" >> archivo_ja.txt
+ echo "3" >> archivo_ja.txt
